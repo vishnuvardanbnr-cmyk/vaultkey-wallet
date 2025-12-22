70555bfed0ac1dbfc193395a25222b0e62c6d49a
vishnuvardanbnr-cmyk <vishnuvardan.bnr@gmail.com>
2025-12-23 03:42:51 +0530

    Register UsbSerialPlugin in MainActivity

--- android/app/src/main/java/app/vaultkey/wallet/MainActivity.java
+++ android/app/src/main/java/app/vaultkey/wallet/MainActivity.java
@@ -1,5 +1,12 @@
 package app.vaultkey.wallet;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(UsbSerialPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
